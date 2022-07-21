cb38acfd5052e1192af020eacd7437927b47b074
Benedict <benedictryan80@gmail.com>
2022-07-21 21:24:48 +0700

    implement infinite query

--- hooks/useFetchData.js
+++ hooks/useFetchData.js
@@ -1,10 +1,11 @@
 import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 import axios from 'axios';
 
-const fetchData = async (url, id) => {
+const fetchData = async (url) => {
 	// const { data } = id ? await axios.get(`${url}/${id}`) : await axios.get(url);
 	const { data } = await axios.get(url);
-	console.log(data);
+
 	return data;
 };
 
@@ -34,5 +35,37 @@ export const useFetchPost = (url, id) => {
 		data,
 		isLoading: !error && !data,
 		error
+	};
+};
+
+export const useInfinitePosts = (url, page) => {
+	// console.log(url);
+
+	const { data, error, isValidating, size, setSize } = useSWRInfinite(
+		(index) => `${url}?_page=${index + 1}&_limit=${5}`,
+		fetchData,
+		{
+			refreshInterval: 1000,
+			keepPreviousData: true
+		}
+	);
+
+	const posts = data ? [].concat(...data) : [];
+	const isLoadingInitialData = !data && !error;
+	const isLoadingMore =
+		isLoadingInitialData ||
+		(size > 0 && data && typeof data[size - 1] === 'undefined');
+	const isEmpty = data?.[0]?.length === 0;
+	const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 5);
+	const isRefreshing = isValidating && data && data.length === size;
+
+	return {
+		posts,
+		error,
+		isLoadingMore,
+		size,
+		setSize,
+		isReachingEnd,
+		isRefreshing
 	};
 };
--- pages/infinite-queries.js
+++ pages/infinite-queries.js
@@ -1,5 +1,44 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { useInfinitePosts } from '../hooks/useFetchData';
+
 function InfiniteQueries() {
-	return <div>InfiniteQueries</div>;
+	const [page, setPage] = useState(1);
+	const {
+		posts,
+		error,
+		isLoadingMore,
+		size,
+		setSize,
+		isReachingEnd,
+		isRefreshing
+	} = useInfinitePosts('https://jsonplaceholder.typicode.com/posts', page);
+
+	return (
+		<div>
+			<h1>SWR Infinite Query</h1>
+			<br />
+			{posts &&
+				posts?.map((post) => (
+					<div key={post.id}>
+						<Link href={`/post/${post.id}`}>
+							<h2>{post.title}</h2>
+						</Link>
+						<p>{post.body}</p>
+					</div>
+				))}
+			<button
+				disabled={isLoadingMore || isReachingEnd}
+				onClick={() => setSize(size + 1)}
+			>
+				{isLoadingMore
+					? 'loading...'
+					: isReachingEnd
+					? 'no more issues'
+					: 'load more'}
+			</button>
+		</div>
+	);
 }
 
 export default InfiniteQueries;
